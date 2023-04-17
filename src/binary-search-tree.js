0e3f5adf3c37f13df1e9051f85abd68a88ccc8a0
foxter6600 <foxter6600@gmail.com>
2023-04-17 23:41:29 +0300

    feat: solve some tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,37 +6,164 @@ const { Node } = require('../extensions/list-tree.js')
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
+
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
 class BinarySearchTree {
-  root() {}
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+  constructor() {
+    this.tree = null
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+  root() {
+    return this.tree
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+  add(data) {
+    this.tree = addData(this.tree, data)
+
+    function addData(node, data) {
+      if (node === null) {
+        return new Node(data)
+      }
+
+      if (node.data === data) {
+        return node
+      }
+
+      if (data > node.data) {
+        node.right = addData(node.right, data)
+      } else {
+        node.left = addData(node.left, data)
+      }
+
+      return node
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented')
+  has(data) {
+    return hasData(this.tree, data)
+
+    function hasData(node, data) {
+      if (node === null) {
+        return false
+      }
+
+      if (node.data === data) {
+        return true
+      }
+
+      if (data > node.data) {
+        return hasData(node.right, data)
+      } else {
+        return hasData(node.left, data)
+      }
+    }
+  }
+
+  find(data) {
+    return findData(this.tree, data)
+
+    function findData(node, data) {
+      if (node === null) {
+        return null
+      }
+
+      if (node.data === data) {
+        return node
+      }
+
+      if (data > node.data) {
+        return findData(node.right, data)
+      } else {
+        return findData(node.left, data)
+      }
+    }
+  }
+
+  remove(data) {
+    // throw new NotImplementedError('Not implemented')
     // remove line with error and write your code here
+    this.tree = removeData(this.tree, data)
+
+    function removeData(node, data) {
+      if (node === null) {
+        return null
+      }
+
+      if (node.data < data) {
+        node.left = removeData(node.left, data)
+        return node
+      } else if (node.data > data) {
+        node.right = removeData(node.right, data)
+        return node
+      } else {
+        if (node.left === null && node.right === null) {
+          return null
+        }
+        if (node.left === null) {
+          node = node.right
+          return node
+        }
+        if (node.right === null) {
+          node = node.left
+          return node
+        }
+
+        let minFromRight = findMinFromRight(node.right)
+
+        function findMinFromRight(node) {
+          if (node.left === null) {
+            return node.value
+          } else {
+            return findMinFromRight(node.left)
+          }
+        }
+
+        node.value = minFromRight
+
+        node.right = removeData(node.right, minFromRight)
+
+        return node
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+    if (this.tree === null) {
+      return null
+    }
+
+    return findMin(this.tree)
+
+    function findMin(node) {
+      if (node.left === null) {
+        return node.data
+      } else {
+        return findMin(node.left)
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+    if (this.tree === null) {
+      return null
+    }
+
+    return findMax(this.tree)
+
+    function findMax(node) {
+      if (node.right === null) {
+        return node.data
+      } else {
+        return findMax(node.right)
+      }
+    }
   }
 }
 
